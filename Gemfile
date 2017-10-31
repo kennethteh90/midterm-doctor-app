a497029f3b965c91bf3a5196797920d814593366
kennethteh90 <kennethteh90@gmail.com>
2017-10-31 10:34:42 +0800

    created Doctors controller, routes, views

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'shoulda-matchers'
 end
 
 group :development do
@@ -52,3 +55,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'simple_form'
+gem 'faker'
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,52 @@
+class DoctorsController < ApplicationController
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to doctors_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    prepare_doctor
+  end
+
+
+  def edit
+    prepare_doctor
+  end
+
+  def update
+    prepare_doctor
+    if @doctor.update(doctor_params)
+      redirect_to doctors_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Doctor.destroy(params[:id])
+  end
+
+  private
+
+    def prepare_doctor
+      @doctor = Doctor.find(params[:id])
+    end
+
+    def doctor_params
+      params.require(:doctor).permit(:username, :address, :gender)
+    end
+
+end
